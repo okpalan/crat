406351f0ab3d8918ddbcf20b164fa72399133a68
nnamdi <nnamdi@NMOUbuntu.myguest.virtualbox.org>
2023-01-04 08:03:23 +0000

    a

--- gif_raytracing/src/main.c
+++ gif_raytracing/src/main.c
@@ -7,6 +7,7 @@
 #include "sphere.h"
 #include "material.h"
 
+
 int main(int argc, char **argv) {
     
 
--- include/jsonp.h
+++ include/jsonp.h
@@ -1,73 +1,41 @@
 #ifndef JSONP_H
 #define JSONP_H
 
+#include <string.h>
 #include <stdio.h>
-#define jsonp_malloc malloc
-#define jsonp_free free
-#define jsonp_strdup strdup
+#include <stdbool.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+enum json_type {
+    JSON_STRING,
+    JSON_NUMBER,
+    JSON_OBJECT,
+    JSON_ARRAY,
+    JSON_TRUE,
+    JSON_FALSE,
+    JSON_NULL
+};
 
-typedef enum {
-    jsonp_type boolean,
-    jsonp_type number,
-    jsonp_type string,
-    jsonp_type array,
-    jsonp_type object
-} jsonp_type;
+struct json_value {
+    enum json_type type;
+    union {
+        char *string;
+        double number;
+        struct json_object *object;
+        struct json_array *array;
+    } data;
+};
 
- typedef struct jsonp_object jsonp_object;
- struct  json_object
-    {
-        struct json_object *_next;
-        struct json_object *_prev;
-        json_type o_type;
-        int _ref_count;
-        union data
-        {
-            struct
-            {
-                char *buf;
-                int len;
-            } _string;
+typedef struct json_object {
+    char *key;
+    struct json_value value;
+    struct json_object *next;
+};
 
-            struct
-            {
-                struct json_object *head;
-                struct json_object *tail;
-            } _object;
+typedef struct json_array {
+    struct json_value value;
+    struct json_array *next;
+};
 
-            struct
-            {
-                double d;
-            } _double;
 
-            struct
-            {
-                int i;
-            } _int;
 
-            struct
-            {
-                int b;
-            } _boolean;
-
-        } o;
-
-    } jsonp_object;
-
-  extern struct json_object *json_object_new_string(const char *s);
-    extern struct json_object *json_object_new_object();
-    extern void json_object_generic_delete(struct json_object *jso);
-    extern int json_object_get_ref_count(struct json_object *jso);
-    extern size_t json_object_to_json_string_len(struct json_object *jso);
-    extern char *json_stringify(json_object *object);
-    extern int json_object_put(struct json_object *jso);
-
-#ifdef __cplusplus
-}
-#endif // __cplusplus
-#endif /* json_h */
-
+#endif // JSONP_H
